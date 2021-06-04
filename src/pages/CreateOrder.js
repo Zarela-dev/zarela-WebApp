7b335016f523ffee152b48c8edfab80c1617539b
Hossein Azadi <hossein.azadi14@outlook.com>
2021-06-04 20:41:47 +0430

    separated createOrderForm from Component

--- src/pages/CreateOrder.js
+++ src/pages/CreateOrder.js
@@ -2,34 +2,13 @@ import React, { useState, useEffect, useContext, useRef } from 'react';
 import { Buffer } from 'buffer';
 import { web3Context } from '../web3Provider';
 import { create } from 'ipfs-http-client';
-import TextField from '../components/Elements/TextField';
 import styled from 'styled-components';
 import TitleBar from '../components/TitleBar';
-import FileInput from '../components/FileInput';
-import Checkbox from '../components/Elements/Checkbox';
-import { Button } from '../components/Elements/Button';
-
-const SubmitButton = styled.button`
-	${Button};
-	width: 260px;
-	height: 50px;
-	margin-bottom: ${props => props.theme.spacing(4)};
-	font-size: 20px;
-	font-weight: 500;
-	color: #252222;
-`;
+import CreateOrderForm from '../components/CreateOrderForm';
 
 const Wrapper = styled.div`
 	max-width: ${props => props.theme.maxWidth};
 	margin: 0 auto;
-`;
-
-const Divider = styled.div`
-	width: 100%;
-	background: rgba(144, 144, 144, 0.3);
-	border-radius: 24px;
-	height: 3px;
-	margin: ${props => props.theme.spacing(4)}  0;
 `;
 
 // #todo sync form data with localStorage
@@ -59,7 +38,7 @@ const CreateOrder = () => {
 
 				// const doc = document.getElementById("_White_Paper");
 				Web3.contract.methods.SetOrderBoard(title, desc, ipfsResponse.path, tokenPay, instanceCount, category)
-					.send({ from: Web3.accounts[0], to: "0xBf23D9f1e75439657c2Cc5ddd200120e265a2ed4"}, (error, result) => {
+					.send({ from: Web3.accounts[0], to: "0xBf23D9f1e75439657c2Cc5ddd200120e265a2ed4" }, (error, result) => {
 						if (!error) {
 							alert(JSON.stringify('Transaction Hash is :  ' + result));
 						}
@@ -99,98 +78,12 @@ const CreateOrder = () => {
 						<div>
 							please connect
 						</div> :
-						<form onSubmit={onSubmit}>
-							<TextField
-								placeholder={'write main topics in your test'}
-								label='Title *'
-								type='text'
-								name={'title'}
-								value={formValues.title}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										title: e.target.value
-									}));
-								}}
-							/>
-							<TextField
-								placeholder={'How many people do you need to done the test?'}
-								label='Description *'
-								type='text'
-								name={'desc'}
-								value={formValues.desc}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										desc: e.target.value
-									}));
-								}}
-							/>
-							<TextField
-								placeholder={'How many biobits will you pay for each contributor?'}
-								label='Allocated Biobits *'
-								type='text'
-								name={'tokenPay'}
-								value={formValues.tokenPay}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										tokenPay: e.target.value
-									}));
-								}}
-							/>
-							<TextField
-								placeholder={'What’s your test about?'}
-								label='Contributors *'
-								type='text'
-								name={'instanceCount'}
-								value={formValues.instanceCount}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										instanceCount: e.target.value
-									}));
-								}}
-							/>
-							<TextField
-								placeholder={'Category'}
-								label='Category *'
-								type='text'
-								name={'category'}
-								value={formValues.category}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										category: e.target.value
-									}));
-								}}
-							/>
-							<FileInput
-								hasBorder={false}
-								showSelected
-								buttonLabel='Upload'
-								label={'Upload your white paper here'}
-								ref={fileRef}
-								name={'whitepaper'}
-								value={formValues.whitepaper}
-								onChange={(e) => {
-									setFormValues(values => ({
-										...values,
-										whitepaper: e.target.value
-									}));
-								}}
-							/>
-							{/* <button type='submit'>
-								submit
-							</button> */}
-							<Divider />
-							<Checkbox checked={formValues.agreement} onChange={(e) => setFormValues(data => ({ ...data, agreement: e.target.checked }))}>
-								Your request won’t be able to be edited, make sure every data you added is correct and final. By marking this box you claim your agreement towards policies.
-							</Checkbox>
-							<SubmitButton>
-								Submit
-							</SubmitButton>
-						</form>
+						<CreateOrderForm
+							onSubmit={onSubmit}
+							formValues={formValues}
+							setFormValues={setFormValues}
+							ref={fileRef}
+						/>
 				}
 			</Wrapper>
 		</>
