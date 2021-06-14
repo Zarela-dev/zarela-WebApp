f3260290b246b6466f49543e6e313d2214ac34f1
Hoss Azadi <azadi.hoss@gmail.com>
2021-06-14 20:25:37 +0430

    solidity contracts added timestamp for signal submission + aggregator functions

--- contracts/CheckPrice.sol
+++ contracts/CheckPrice.sol
@@ -4,30 +4,29 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorInterface.sol";
+import "./AggregatorInterface.sol";
 
 contract PriceConsumer {
 
     AggregatorInterface internal priceFeed;
+    AggregatorInterface internal BTCFeed;
 
     /**
      * Network: Kovan
      * Aggregator: ETH/USD
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
      */
-    constructor()  {
+    constructor() {
         priceFeed = AggregatorInterface(0x9326BFA02ADD2366b30bacB125260Af641031331);
     }
-  
+    int ETHPrice;
 
     uint LastPrice;
     function getLatestPrice()public payable {
         LastPrice = ( 500000 * tx.gasprice * (uint(priceFeed.latestAnswer()) / 100000000) ) / 1000000000000000000 ;
     }
-    /**
-     * Returns the timestamp of the latest price update
-     */
-    function getLatestPriceTimestamp() public view returns (uint256) {
-        return priceFeed.latestTimestamp();
+    
+    function GetETHPrice()public view returns(int256) {
+        return priceFeed.latestAnswer();
     }
 }
