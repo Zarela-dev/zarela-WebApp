6a6088133de868dd55ca9dfd69c1e236ef543693
Hoss Azadi <azadi.hoss@gmail.com>
2021-07-26 17:40:17 +0430

    added "switch device" message in create request

--- src/pages/CreateRequest.js
+++ src/pages/CreateRequest.js
@@ -14,6 +14,8 @@ import { Persist } from 'formik-persist';
 import { toast } from '../utils';
 import Dialog from '../components/Dialog';
 import { useWeb3React } from '@web3-react/core';
+import deviceContext from '../utils/context';
+import NoMobileSupportMessage from '../components/NoMobileSupportMessage';
 
 const Wrapper = styled.div`
 	${maxWidthWrapper}
@@ -28,21 +30,21 @@ const CreateRequest = () => {
 	const [isUploading, setUploading] = useState(false);
 	const [dialogMessage, setDialogMessage] = useState('');
 	const { account } = useWeb3React();
+	const { device } = useContext(deviceContext);
 
 	const clearSubmitDialog = () => {
 		setUploading(false);
 		setDialogMessage('');
 		formik.setSubmitting(false);
 
-		if (fileRef.current !== null)
-			fileRef.current.value = null;
+		if (fileRef.current !== null) fileRef.current.value = null;
 	};
 	const validationErrors = {
-		required: name => `${name} is required to create your request.`,
+		required: (name) => `${name} is required to create your request.`,
 		notEnoughTokens: 'you do not have enough tokens to create this request.',
 		terms: 'you must agree to the term before submitting your request',
-		number: name => `${name} must be a number`,
-		string: name => `${name} must be a string`,
+		number: (name) => `${name} must be a number`,
+		string: (name) => `${name} must be a string`,
 	};
 
 	const formik = useFormik({
@@ -53,20 +55,33 @@ const CreateRequest = () => {
 			instanceCount: '',
 			category: '',
 			zpaper: '',
-			terms: false
+			terms: false,
 		},
 		validationSchema: yup.object().shape({
-			title: yup.string(validationErrors.string('title')).required(validationErrors.required('title')),
-			desc: yup.string(validationErrors.string('description')).required(validationErrors.required('description')),
-			tokenPay: yup.number().typeError(validationErrors.number('Allocated Biobits')).required(validationErrors.required('Allocated Biobit')),
-			instanceCount: yup.number().typeError(validationErrors.number('Contributors')).required(validationErrors.required('Contributors')),
+			title: yup
+				.string(validationErrors.string('title'))
+				.required(validationErrors.required('title')),
+			desc: yup
+				.string(validationErrors.string('description'))
+				.required(validationErrors.required('description')),
+			tokenPay: yup
+				.number()
+				.typeError(validationErrors.number('Allocated Biobits'))
+				.required(validationErrors.required('Allocated Biobit')),
+			instanceCount: yup
+				.number()
+				.typeError(validationErrors.number('Contributors'))
+				.required(validationErrors.required('Contributors')),
 			category: yup.string().required(validationErrors.required('category')),
 			zpaper: yup.mixed(),
-			terms: yup.boolean().required()
+			terms: yup.boolean().required(),
 		}),
-		onSubmit: (values => {
+		onSubmit: (values) => {
 			if (formik.isValid) {
-				if (+values.tokenPay * +values.instanceCount > +appState.biobitBalance / Math.pow(10, 9)) {
+				if (
+					+values.tokenPay * +values.instanceCount >
+					+appState.biobitBalance / Math.pow(10, 9)
+				) {
 					formik.setFieldError('tokenPay', validationErrors.notEnoughTokens);
 					formik.setSubmitting(false);
 				} else {
@@ -78,7 +93,9 @@ const CreateRequest = () => {
 							setDialog(false);
 							if (fileRef.current.value !== null && fileRef.current.value !== '') {
 								setUploading(true);
-								setDialogMessage('in request to secure the file, so only you can access it we require your public key to encrypt the file');
+								setDialogMessage(
+									'in request to secure the file, so only you can access it we require your public key to encrypt the file'
+								);
 
 								const { title, desc, tokenPay, instanceCount, category } = values;
 								const reader = new FileReader();
@@ -101,25 +118,51 @@ const CreateRequest = () => {
 											try {
 												const ipfsResponse = await ipfs.add(buf);
 												formik.setFieldValue('zpaper', ipfsResponse.path);
-												let url = `${process.env.REACT_APP_IPFS_LINK + ipfsResponse.path}`;
+												let url = `${
+													process.env.REACT_APP_IPFS_LINK +
+													ipfsResponse.path
+												}`;
 												console.log(`Document Of Conditions --> ${url}`);
 
 												setDialogMessage('awaiting confirmation');
 
-												appState.contract.methods.SetOrderBoard(title, desc, ipfsResponse.path, +tokenPay * Math.pow(10, 9), instanceCount, category, encryptionPublicKey)
-													.send({ from: account, to: process.env.REACT_APP_ZARELA_CONTRACT_ADDRESS, gasPrice: +appState.gas.average * Math.pow(10, 8) }, (error, result) => {
-														if (!error) {
-															clearSubmitDialog();
-															toast(result, 'success', true, result);
+												appState.contract.methods
+													.SetOrderBoard(
+														title,
+														desc,
+														ipfsResponse.path,
+														+tokenPay * Math.pow(10, 9),
+														instanceCount,
+														category,
+														encryptionPublicKey
+													)
+													.send(
+														{
+															from: account,
+															to: process.env
+																.REACT_APP_ZARELA_CONTRACT_ADDRESS,
+															gasPrice:
+																+appState.gas.average *
+																Math.pow(10, 8),
+														},
+														(error, result) => {
+															if (!error) {
+																clearSubmitDialog();
+																toast(
+																	result,
+																	'success',
+																	true,
+																	result
+																);
+															} else {
+																clearSubmitDialog();
+																toast(error.message, 'error');
+															}
 														}
-														else {
-															clearSubmitDialog();
-															toast(error.message, 'error');
-														}
-													});
+													);
 
-
-												appState.contract.events.OrderRegistered({})
+												appState.contract.events
+													.OrderRegistered({})
 													.on('data', (event) => {
 														clearSubmitDialog();
 
@@ -129,11 +172,12 @@ const CreateRequest = () => {
 															false,
 															null,
 															{
-																toastId: event.id
+																toastId: event.id,
 															}
 														);
-														history.push(`/request/${event.returnValues[1]}`);
-
+														history.push(
+															`/request/${event.returnValues[1]}`
+														);
 													})
 													.on('error', (error, receipt) => {
 														clearSubmitDialog();
@@ -150,7 +194,9 @@ const CreateRequest = () => {
 										if (error.code === 4001) {
 											// EIP-1193 userRejectedRequest error
 											clearSubmitDialog();
-											console.log("We can't encrypt anything without the key.");
+											console.log(
+												"We can't encrypt anything without the key."
+											);
 										} else {
 											console.error(error);
 										}
@@ -164,7 +210,7 @@ const CreateRequest = () => {
 					}
 				}
 			}
-		})
+		},
 	});
 
 	useEffect(() => {
@@ -176,35 +222,33 @@ const CreateRequest = () => {
 
 	return (
 		<>
-			<TitleBar>
-				Create Request
-			</TitleBar>
+			<TitleBar>Create Request</TitleBar>
 			<Wrapper>
-				{
+				{device === 'Mobile' ? (
+					<NoMobileSupportMessage />
+				) : (
 					<>
 						<Dialog
 							isOpen={isUploading}
-							content={(
-								dialogMessage
-							)}
+							content={dialogMessage}
 							onClose={() => {
 								clearSubmitDialog();
 							}}
 							hasSpinner
-							type='success'
+							type="success"
 						/>
-						<ConnectDialog isOpen={showDialog} onClose={() => {
-							formik.setSubmitting(false);
-							setDialog(false);
-						}} />
-						<CreateRequestForm
-							formik={formik}
-							ref={fileRef}
-						>
+						<ConnectDialog
+							isOpen={showDialog}
+							onClose={() => {
+								formik.setSubmitting(false);
+								setDialog(false);
+							}}
+						/>
+						<CreateRequestForm formik={formik} ref={fileRef}>
 							<Persist />
 						</CreateRequestForm>
 					</>
-				}
+				)}
 			</Wrapper>
 		</>
 	);
