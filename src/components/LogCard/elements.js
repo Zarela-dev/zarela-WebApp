07569ffac8a88bcc7ee61063956c400ffeae68f2
Hoss Azadi <azadi.hoss@gmail.com>
2021-08-08 17:05:35 +0430

    part of request card in log > contributes refactor

--- src/components/LogCard/elements.js
+++ src/components/LogCard/elements.js
@@ -1,152 +1,106 @@
-import {
-	RequestNumber,
-	HeaderLayout,
-	Footer,
-	Spacer,
-	TokenIcon,
-	Bookmark,
-	Typography,
-	ContributorBadge,
-	ContributorsIcon,
-	ProgressTrackerWrapper,
-	TokenValue,
-	Divider,
-	BadgeRow,
-	Title,
-	BiobitToDollarValue,
-	BadgeLabel,
-} from '../Elements/RequestCard/IndexMobile';
-import maxWidthWrapper from '../Elements/MaxWidth';
 import styled from 'styled-components';
 
-export const CustomRequestNumber = styled(RequestNumber)`
-	flex: ${(props) => (props.isMobile ? '' : '0 0 100px')};
-	height: ${(props) => (props.isMobile ? '26px;' : '50px')};
-	border-radius: ${(props) => (props.isMobile ? '5px 5px 0px 5px' : '10px 10px 0px 10px')};
-	padding: ${(props) => (props.isMobile ? '7px 11px' : '10px 20px')};
-	margin-right: ${(props) => (props.isMobile ? props.theme.spacing(1) : props.theme.spacing(2))};
-	font-weight: bold;
-	background: linear-gradient(246.29deg, #3a68de 12.69%, #3a68de 100%);
-	font-size: ${(props) => (props.isMobile ? '15px' : '32px')};
-	line-height: ${(props) => (props.isMobile ? '11.4px' : '30px')};
-	color: #ffffff;
-	text-align: center;
-`;
-
-export const HeaderContainer = styled.header`
+// layout
+export const CompactRequestCard = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	width: 100%;
+	min-height: 75px;
 	background: #f4f8fe;
 	border-radius: 8px;
-	padding: 13px 21px;
-	width: 100%;
-	margin-bottom: 10px;
+	margin-bottom: ${(props) => props.theme.spacing(1)};
+	padding: ${(props) => `${props.theme.spacing(1.3)} ${props.theme.spacing(2)}`};
 `;
 
-export const HeaderInner = styled(HeaderLayout)`
+export const Row = styled.div`
 	display: flex;
-	flex-direction: ${(props) => (props.isMobile ? 'column' : 'row')};
-	${maxWidthWrapper};
-	width: 100%;
-	justify-content: space-between;
-`;
-
-export const HeaderLayoutCustom = styled(HeaderLayout)`
-	width: unset;
-	flex-grow: 1;
-`;
-
-export const CustomFooter = styled(Footer)`
-	margin-top: ${(props) => (props.isMobile ? props.theme.spacing(3) : props.theme.spacing(0))};
-	padding-left: ${(props) => (props.isMobile ? '50px' : '0')};
+	align-items: center;
 	flex-wrap: nowrap;
+	flex-direction: row;
+	flex: 1 0 100%;
 `;
 
-export const CustomBadgeRow = styled(BadgeRow)`
-	flex: 0;
+export const Header = styled(Row)``;
+export const Body = styled(Row)``;
+
+export const Column = styled.div`
+	display: ${(props) => (props.displayFlex ? 'flex' : 'block')};
 	align-items: center;
+	align-self: ${(props) => props.alignSelf || 'center'};
+	flex: ${(props) => props.flex || 1};
 `;
+// icons
+export const Icon = styled.img``;
 
-export const CustomBadgeRowGrow = styled(CustomBadgeRow)`
-	flex-grow: 1;
-`;
-
-export const TitleContent = styled(Title)`
-	font-size: ${(props) => (props.isMobile ? '12px' : '14px')};
-	font-weight: ${(props) => (props.isMobile ? '400' : '600')};
-	line-height: ${(props) => (props.isMobile ? '16px' : '20px')};
-	font-weight: 600;
-	line-heigh: 16px;
-`;
-
-export const CustomDivider = styled(Divider)`
-	height: 48px;
-	background-color: rgba(60, 135, 170, 0.6);
-	margin: 0 20px;
-`;
-
-export const BodyContainer = styled.div`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	margin-top: ${(props) => (props.isMobile ? '10px' : '8px')};
-`;
-
-export const HashContent = styled.div`
-	display: flex;
-	flex-direction: ${(props) => (props.isMobile ? 'column' : 'row')};
-`;
-
-export const HashBody = styled.div`
-	word-break: break-all;
-	font-size: 12px;
-	font-weight: 500;
-	color: #3a68de;
-	line-height: 16px;
-`;
-
-export const HashTitle = styled(HashBody)`
-	font-size: ${(props) => (props.isMobile ? '10.5px' : '14px')};
-	font-weight: 400;
-	line-height: ${(props) => (props.isMobile ? '16px' : '20px')};
-	color: ${(props) => (props.isMobile ? '' : '#000')};
-	margin-right: ${(props) => (props.isMobile ? '' : '6px')};
-	align-items: center;
-`;
-
-export const CustomContributorBadge = styled(ContributorBadge)`
-	margin-right: ${(props) => (props.isMobile ? '8px' : '25px')};
-`;
-
-export const ContributorsIconMobile = styled(ContributorsIcon)`
-	width: ${(props) => (props.isMobile ? '16px' : '18px')};
-	height: ${(props) => (props.isMobile ? '16px' : '18px')};
-`;
-export const ToeknIconMobile = styled(TokenIcon)`
+export const BiobitIcon = styled(Icon)`
 	width: 20px;
-	height: 20px;
+	margin-right: 5px;
 `;
 
-export const BadgeLabelTimeMobile = styled(BadgeLabel)`
-	font-size: ${(props) => (props.isMobile ? '10.8px' : '14px')};
+export const CaretIcon = styled(Icon)`
+	width: 24px;
+	margin-left: ${(props) => props.theme.spacing(1)};
+`;
+export const StatusIcon = styled(Icon)`
+	width: 20px;
+	margin-right: ${(props) => props.theme.spacing(1)};
+`;
+export const ContributorIcon = styled(Icon)``;
+export const BookmarkIcon = styled(Icon)``;
+
+// icon labels
+export const StatusLabel = styled.p`
+	font-weight: bold;
+	font-size: 16px;
 	line-height: 20px;
-	font-weight: ${(props) => (props.success ? '700' : props.pending && '700')};
-	color: ${(props) =>
-		props.success ? 'rgba(27, 204, 141, 1)' : props.pending && 'rgba(133, 133, 133, 1)'};
+	color: ${(props) => (props.approved ? '#1bcc8d' : '#858585')};
 `;
 
-export const BadgeLabelContributors = styled(BadgeLabel)`
-	font-size: ${(props) => (props.isMobile ? '10.8px' : '16px')};
-	line-height: ${(props) => (props.isMobile ? '14px' : '18px')};
-	font-weight: 400;
-	min-width: ${(props) => props.minWidth && '60px'};
+export const BiobitValue = styled.p`
+	white-space: nowrap;
+`;
+export const DollarValue = styled.p`
 	white-space: nowrap;
 `;
 
-export const CopyIconWrapper = styled(Bookmark)`
-	align-self: center;
+export const VerticalDivider = styled.div`
+	height: 50px;
+	width: 1px;
+	border-right: 1px solid rgba(60, 135, 170, 0.6);
+	margin: 0 ${(props) => props.theme.spacing(2)};
 `;
 
-export const ContentWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+export const ContributorCount = styled.p``;
+
+export const Title = styled.h4`
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 1.8;
+	color: ${(props) => props.theme.textPrimary};
+	margin-right: ${(props) => props.theme.spacing(1.5)};
+	margin-bottom: ${(props) => props.theme.spacing(0.8)};
+`;
+
+export const Timestamp = styled.p``;
+
+export const QuickReport = styled.p`
+	font-weight: ${(props) => (props.bold ? 'bold' : 'normal')};
+	font-size: 12px;
+	line-height: 16px;
+	color: #d13ade;
+`;
+
+export const RequestNumber = styled.div`
+	height: 60px;
+	border-radius: 10px 10px 0px 10px;
+	padding: 0 20px;
+	margin-right: ${(props) => props.theme.spacing(2)};
+	font-weight: 700;
+	background: linear-gradient(246.29deg, #3a68de 12.69%, #3a68de 100%);
+	font-size: 32px;
+	line-height: 60px;
+	color: #ffffff;
+	text-align: center;
+	user-select: text;
 `;
