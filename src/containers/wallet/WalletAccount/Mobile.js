6928ba5ba43d06df57e0175c3b7a298c7e0d3046
Hoss Azadi <azadi.hoss@gmail.com>
2021-08-10 18:37:53 +0430

    added note in wallet > account

--- src/containers/wallet/WalletAccount/Mobile.js
+++ src/containers/wallet/WalletAccount/Mobile.js
@@ -86,13 +86,9 @@ const WalletAccountMobile = ({ address }) => {
 					</MobileColumn>
 				</MobileColumn>
 			</ContentMobile>
-			<Note title={'Important'}>
-				dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-				text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-				specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-				remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-				containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-				including versions of Lorem Ipsum.
+			<Note title={'Advice'}>
+				By copying your account address and sending it to others, you can easily have financial transactions,
+				including Biobit, Ethereum, etc. Also, You must never give your private keys to anyone.
 			</Note>
 		</WrapperMobile>
 	);
--- src/containers/wallet/WalletAccount/index.js
+++ src/containers/wallet/WalletAccount/index.js
@@ -1,19 +1,12 @@
-import React from "react";
-import Note from "../../../components/Note";
-import styled from "styled-components";
-import nextStepIcon from "./../../../assets/icons/next-step-arrow.svg";
-import qrCodeImage from "../../../assets/icons/qr-code.png";
-import { CopyableText } from "../../../utils";
-import { Wrapper, Content, Row, Column } from "./Layout";
-import {
-	QRCode,
-	QRCodeImage,
-	SaveQRCodeButton,
-	AddressTitle,
-	Address,
-	CopyAddressButton,
-} from "./WalletInfo";
-import WalletsList from "../../../components/IntroModal/WalletsList";
+import React from 'react';
+import Note from './../../../components/Note';
+import styled from 'styled-components';
+import nextStepIcon from './../../../assets/icons/next-step-arrow.svg';
+import qrCodeImage from '../../../assets/icons/qr-code.png';
+import { CopyableText } from './../../../utils';
+import { Wrapper, Content, Row, Column } from './Layout';
+import { QRCode, QRCodeImage, SaveQRCodeButton, AddressTitle, Address, CopyAddressButton } from './WalletInfo';
+import WalletsList from '../../../components/IntroModal/WalletsList';
 
 const NextStepArrow = styled.div`
 	height: 100%;
@@ -26,13 +19,13 @@ const NextStepArrow = styled.div`
 	margin: 0 30px;
 
 	&::before {
-		content: "";
+		content: '';
 		height: 50%;
 		width: 1px;
 		background: #c4c4c4;
 	}
 	&::after {
-		content: "";
+		content: '';
 		height: 50%;
 		width: 1px;
 		background: #c4c4c4;
@@ -64,31 +57,21 @@ const WalletAccount = ({ address }) => {
 							<QRCode>
 								<QRCodeImage src={qrCodeImage} />
 							</QRCode>
-							<SaveQRCodeButton variant="secondary">
-								save QR code
-							</SaveQRCodeButton>
+							<SaveQRCodeButton variant="secondary">save QR code</SaveQRCodeButton>
 						</Column>
 						<Column center>
 							<AddressTitle>BBit account address</AddressTitle>
 							<Address>{address}</Address>
 							<CopyableText textToCopy={address}>
-								<CopyAddressButton variant="secondary">
-									Copy address
-								</CopyAddressButton>
+								<CopyAddressButton variant="secondary">Copy address</CopyAddressButton>
 							</CopyableText>
 						</Column>
 					</Row>
 					<Row>
-						<Notice title={"Important"}>
-							dummy text of the printing and typesetting industry. Lorem Ipsum
-							has been the industry's standard dummy text ever since the 1500s,
-							when an unknown printer took a galley of type and scrambled it to
-							make a type specimen book. It has survived not only five
-							centuries, but also the leap into electronic typesetting,
-							remaining essentially unchanged. It was popularised in the 1960s
-							with the release of Letraset sheets containing Lorem Ipsum
-							passages, and more recently with desktop publishing software like
-							Aldus PageMaker including versions of Lorem Ipsum.
+						<Notice title={'Advice'}>
+							By copying your account address and sending it to others, you can easily have financial
+							transactions, including Biobit, Ethereum, etc. Also, You must never give your private
+							keys to anyone.
 						</Notice>
 					</Row>
 				</Column>
