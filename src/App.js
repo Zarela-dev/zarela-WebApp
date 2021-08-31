6b025d3aae6a0b908f2ecef8058f675ff8d02eee
Miladjamali <jamalimilad60@gmail.com>
2021-08-31 17:56:38 +0430

    data connection and badge styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Web3ReactProvider } from '@web3-react/core';
 import AppRouter from './Router';
 import ToastifyContainer from './components/ToastifyContainer';
@@ -10,23 +10,8 @@ import { theme } from './theme';
 import getLibrary from './utils/getLibrary';
 import ErrorBoundary from './ErrorBoundary';
 import 'react-toastify/dist/ReactToastify.css';
-import {log , toast} from './utils'
 
 function App() {
-	useEffect(() => {
-		log(
-			`signal submitted on requesefe signal submitted on requesefe signal submitted on requesefe`,
-			'success',
-			false,
-			null,
-		);
-		toast(
-			`signal submitted on requesefe`,
-			'success',
-			false,
-			null,
-		);
-	})
 	return (
 		<div className='App'>
 			<ErrorBoundary>
