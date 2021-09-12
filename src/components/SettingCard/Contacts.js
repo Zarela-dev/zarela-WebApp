fd684ef5f08cbcc61c6e58544fb3c01bac98022a
Miladjamali <jamalimilad60@gmail.com>
2021-09-12 18:46:57 +0430

    localStorageContact

--- src/components/SettingCard/Contacts.js
+++ src/components/SettingCard/Contacts.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import deleteIcon from '../../assets/icons/actionIcons/delete.svg';
 import edit from '../../assets/icons/actionIcons/edit.svg';
 import block from '../../assets/icons/actionIcons/block.svg';
+import { localStorageContext } from '../../state/localStorageProvider/LocalStoragePriveder';
+import { addContact, blockAddress, hideAddress, removeContact, unBlockAddress, unHideAddress } from '../../state/localStorageProvider/actions';
 
 import {
 	CompactRequestCard,
@@ -30,6 +32,9 @@ const ActionTitle = styled.span`
 `;
 
 const Contacts = ({ isMobile }) => {
+
+	const { localState , dispatch } = useContext(localStorageContext);
+
 	if (isMobile) {
 		return (
 			<>
@@ -41,6 +46,20 @@ const Contacts = ({ isMobile }) => {
 		return (
 			<CompactRequestCard>
 				<Body>
+					<div>
+						{/* <button onClick={() => addContact(dispatch , 'publickeyExmaple' , 'name' )}>add contact</button>
+						<button onClick={() => removeContact(dispatch , 'publickeyExmaple' )}>remove contact</button> */}
+						{/* <button onClick={() => blockAddress(dispatch , 'one')}>block contact</button>
+						<button onClick={() => blockAddress(dispatch , 'two')}>block contact</button>
+						<button onClick={() => unBlockAddress(dispatch , 'one' )}>unblock contact</button> */}
+						<button onClick={() => hideAddress(dispatch , 'one' , 2 )}>hide contact</button>
+						<button onClick={() => hideAddress(dispatch , 'one' , 2 )}>hide contact</button>
+						<button onClick={() => hideAddress(dispatch , 'one' , 3 )}>hide contact</button>
+						<button onClick={() => hideAddress(dispatch , 'one' , 3 )}>hide contact</button>
+						<button onClick={() => hideAddress(dispatch , 'one' , 3 )}>hide contact</button>
+						<button onClick={() => hideAddress(dispatch , 'one' , 2 )}>hide contact</button>
+						<button onClick={() => unHideAddress(dispatch , 'one' , 2 )}>unhide contact</button>
+						</div>
 					<Table>
 						<TableRow header>
 							<SettingTableCell flex="0 0 35%">
---
+++ src/state/localStorageProvider/LocalStoragePriveder.js
@@ -0,0 +1,115 @@
+import React, { useReducer, useEffect } from 'react';
+import { actionTypes } from './actionTypes';
+import * as lockr from 'lockr';
+import _ from 'lodash';
+import { addContact, removeContact } from './actions';
+import { useDeepCompareEffect } from 'use-deep-compare';
+
+const appInitialState = {
+	contacts: {},
+	blockList: [],
+	hideList: {},
+};
+
+const localStorageContext = React.createContext(appInitialState);
+
+const LocalStorageProvider = ({ children }) => {
+	const [localState, dispatch] = useReducer((state, action) => {
+		const { type } = action;
+		switch (type) {
+			case actionTypes.SET_LOCALSTATE:
+				return action.payload;
+
+			case actionTypes.ADD_CONTACT: {
+				const { publicKey, alias } = action.payload;
+				return {
+					...state,
+					contacts: {
+						...state.contacts,
+						[publicKey]: alias,
+					},
+				};
+			}
+			case actionTypes.REMOVE_CONTACT: {
+				const { publicKey } = action.payload;
+				let contacts = _.omit(state.contacts, publicKey);
+				return {
+					...state,
+					contacts,
+				};
+			}
+
+			case actionTypes.BLOCK_ADDRESS: {
+				const { publicKey } = action.payload;
+				return {
+					...state,
+					blockList: [...state.blockList, publicKey],
+				};
+			}
+
+			case actionTypes.UNBLOCK_ADDRESS: {
+				const { publicKey } = action.payload;
+
+				let blockList = state.blockList.filter((item) => item !== publicKey);
+				return {
+					...state,
+					blockList,
+				};
+			}
+
+			case actionTypes.HIDE_ADDRESS: {
+				const { publicKey, orderId } = action.payload;
+				const requestIdSet = new Set(
+					state.hideList[publicKey] ? [...state.hideList[publicKey], orderId] : [orderId]
+				);
+				return {
+					...state,
+					hideList: {
+						...state.hideList,
+						[publicKey]: [...requestIdSet],
+					},
+				};
+			}
+
+			case actionTypes.UNHIDE_ADDRESS: {
+				const { publicKey, orderId } = action.payload;
+				let hideList = state.hideList[publicKey].filter((item) => item !== orderId);
+				return {
+					...state,
+					hideList: {
+						...state.hideList,
+						[publicKey]: [...hideList],
+					},
+				};
+			}
+			default:
+				return state;
+		}
+	}, appInitialState);
+
+	useDeepCompareEffect(() => {
+		if (!_.isEmpty(localState)) lockr.set('contacts_list', localState);
+		else lockr.flush('contacts_list');
+	}, [localState]);
+
+	useEffect(() => {
+		let localStorageData = lockr.get('contacts_list');
+		dispatch({
+			type: actionTypes.SET_LOCALSTATE,
+			payload: localStorageData,
+		});
+	}, []);
+
+	return (
+		<localStorageContext.Provider
+			value={{
+				localState,
+				dispatch,
+			}}
+		>
+			{children}
+		</localStorageContext.Provider>
+	);
+};
+
+export { localStorageContext, LocalStorageProvider };
