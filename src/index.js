6664520473674a4eaf8f70a43e203857dd426520
Hoss Azadi <azadi.hoss@gmail.com>
2021-09-16 17:42:21 +0430

    added GTM only in testnet build

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module';
+
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_IS_TEST_NET === 'true') {
+	const tagManagerArgs = {
+		gtmId: 'GTM-M2G7MQ6',
+	};
+	TagManager.initialize(tagManagerArgs);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
