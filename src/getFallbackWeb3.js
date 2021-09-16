0eaa3400ed9ad3a65aafcfa5057d1c464c18da9a
Hoss Azadi <azadi.hoss@gmail.com>
2021-09-16 23:31:11 +0430

    removed local bloackchain connection

--- src/getFallbackWeb3.js
+++ src/getFallbackWeb3.js
@@ -20,13 +20,6 @@ const getWeb3 = async () =>
 				console.log('Injected web3 detected.');
 				resolve(web3);
 			}
-			// Fallback to localhost; use dev console port by default
-			else {
-				const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
-				const web3 = new Web3(provider);
-				console.log('No web3 instance injected, using Local web3.');
-				resolve(web3);
-			}
 		});
 	});
 
