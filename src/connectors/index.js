37b517ca75344580130b2569327ccea8c8ff1ade
Hoss Azadi <azadi.hoss@gmail.com>
2021-09-24 17:07:25 +0330

    removed walletConnect connector

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -1,13 +1,6 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { supportedChains } from '../constants';
 
 export const injectedConnector = new InjectedConnector({
 	supportedChainIds: [supportedChains.ROPSTEN]
-});
-
-export const walletConnectConnector = new WalletConnectConnector({
-	supportedChainIds: [supportedChains.ROPSTEN],
-	qrcode: true,
-	infuraId: process.env.REACT_APP_INFURA_ID
 });
