27fc5e358afbf2681a667d36360561bc02d2117d
Hoss Azadi <azadi.hoss@gmail.com>
2022-01-05 15:38:30 +0330

    added Multisend SmartContract

---
+++ contracts/MultiSendToken.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.16;
+
+/**
+ * token contract functions
+*/
+contract Token {
+    function balanceOf(address who) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
+    function approveAndCall(address spender, uint tokens, bytes data) external returns (bool success);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    require(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+
+  function ceil(uint256 a, uint256 m) internal constant returns (uint256) {
+    uint256 c = add(a,m);
+    uint256 d = sub(c,1);
+    return mul(div(d,m),m);
+  }
+}
+
+
+contract MultiSend {
+    using SafeMath for uint256;
+
+    function MultiSendTokens(address _tokenAddress, address[] memory _addresses ,uint256[] memory _amount) public {
+        require(_amount.length == _addresses.length);
+        require(_addresses.length > 0);
+        for(uint i ; i < _addresses.length ; i++){
+            Token(_tokenAddress).transferFrom(msg.sender, _addresses[i], _amount[i]);
+        }
+        
+    
+    }
+    
+}
